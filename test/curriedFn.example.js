540b10218d98066c86812ccad9fe5d28e0b4d404
kghugo <howtostudycantonese@gmail.com>
2020-10-04 18:30:11 +0800

    :white_check_mark: Add test expection for uncurrying curried functions

--- test/curriedFn.example.js
+++ test/curriedFn.example.js
@@ -1,12 +1,19 @@
 const ES6CurriedFn = (param1) => (param2) => param1
 
+const ES6UncurriedFn = (param1, param2) => param1
+
 function ES5CurriedFn (param1) {
   return function (param2) {
     return param1
   }
 }
 
+function ES5UncurriedFn (param1, param2) {
+  return param1
+}
+
 export {
   ES6CurriedFn,
+  ES6UncurriedFn,
   ES5CurriedFn
 }
--- test/curriedFn.spec.js
+++ test/curriedFn.spec.js
@@ -2,14 +2,12 @@ import * as uncurry from '../src/index.js'
 import {
   rollup
 } from 'rollup'
+const acorn = require('acorn')
+const walk = require('acorn-walk')
+const chai = require('chai')
+const expect = chai.expect
 
 describe('Uncurrying for curried function', function () {
-  // let exampleScript
-  //
-  // beforeEach(function () {
-  //   exampleScript = ''
-  // })
-
   it('should uncurry that curried function', async function () {
     const bundle = await rollup({
       input: './test/curriedFn.example.js',
@@ -26,6 +24,28 @@ describe('Uncurrying for curried function', function () {
       ]
     })
 
-    console.log(output)
+    const outputCode = output[0].code
+
+    const outputAST = acorn.parse(outputCode, {
+      sourceType: 'module'
+    })
+
+    walk.simple(
+      outputAST,
+      {
+        VariableDeclaration (node) {
+          if (node.declarations[0].id.name === 'ES6CurriedFn') {
+            const arrowFn = node.declarations[0].init
+
+            // expect(arrowFn.params).to.have.lengthOf(2)
+          }
+        },
+        FunctionDeclaration (node) {
+          if (node.id.name === 'ES5CurriedFn') {
+            // expect(node.params).to.have.lengthOf(2)
+          }
+        }
+      }
+    )
   })
 })
